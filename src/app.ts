8603646e2fd27cabf3e26e5cd5da9d1e305afbfc
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-04-12 21:25:44 -0400

    Add another test input file

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import {readFileSync} from "fs";
 import { jsonParse } from "./json-parser";
 
 /* Edit here to change the file path to the json file */
-const FILE_PATH = "medium_test.json";
+const FILE_PATH = "easy_test.json";
 
 const jsonStr = readFileSync(FILE_PATH, "utf8");
 const result = jsonParse(jsonStr);
